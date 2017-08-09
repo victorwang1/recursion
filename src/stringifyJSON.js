d7b0ea2eb6a2900f65f0f460387a7a26970cec5f
Victor Wang <victor.wang@me.com>
2017-08-09 19:32:32 +1000

    delete unnecessary code blocks

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -8,10 +8,6 @@ var stringifyJSON = function(obj) {
 
   // array
   if (Array.isArray(obj)) {
-    if (obj.length === 0) {
-      return '[]';
-    }
-
     var array = [];
     for (let value of obj) {
       array.push(stringifyJSON(value));
@@ -21,12 +17,7 @@ var stringifyJSON = function(obj) {
 
   // object
   else if (typeof obj === 'object') {
-    if (obj === null) {
-      return 'null';
-    }
-    if (Object.keys(obj).length === 0) {
-      return '{}';
-    }
+    if (obj === null) return 'null';
     var array = [];
     for (let key in obj) {
       if (stringifyJSON(obj[key]) !== '') {
@@ -46,10 +37,8 @@ var stringifyJSON = function(obj) {
     return '';
   }
 
-  // fallback
-  if (output === '') {
-    return obj.toString();
-  }
+  // primitive values and fallback
+  if (output === '') return obj.toString();
 
   return output;
 };
